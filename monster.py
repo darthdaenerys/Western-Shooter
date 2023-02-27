98efbee56fade93890154f14e26a3598d8dd024d
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-02-27 19:53:56 +0530

    Update Monster method

--- monster.py
+++ monster.py
@@ -5,4 +5,8 @@ class Monster:
     def get_player_distance_direction(self):
         distance=(vector(self.player.hitbox.center)-vector(self.hitbox.center)).magnitude()
         direction=None
-        
+        if distance!=0:
+            direction=(vector(self.player.hitbox.center)-vector(self.hitbox.center)).normalize()
+        else:
+            direction=vector()
+        return distance,direction
