e08699b3f2c0d0464b7c31bef25d596167f77c13
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-03-01 19:43:41 +0530

    Add monster chase method

--- monster.py
+++ monster.py
@@ -10,3 +10,12 @@ class Monster:
         else:
             direction=vector()
         return distance,direction
+    
+    def chase(self):
+        distance,direction=self.get_player_distance_direction()
+        if self.attack_radius<distance<self.walk_radius:
+            self.direction=direction
+            self.status=self.status.split('_')[0]
+        else:
+            self.direction=vector()
+    
